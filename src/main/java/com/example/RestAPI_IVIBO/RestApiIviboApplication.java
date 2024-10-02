d0c68bf97722561053e54cab3e74890035115907
basil204 <96483932+basil204@users.noreply.github.com>
2024-10-02 13:42:15 +0700

    mau

---
+++ src/main/java/com/example/RestAPI_IVIBO/RestApiIviboApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestAPI_IVIBO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiIviboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiIviboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestAPI_IVIBO/RestApiIviboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestAPI_IVIBO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiIviboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
