fc79dc1e5b0e83d1c63dab95763f50dbf64a5a82
huyv02961 <huyvhpp02961@fpt.edu.vn>
2024-10-02 15:37:09 +0700

    a

---
+++ src/main/java/com/example/RestAPI_IVIBO/Repositories/LichTrinhRepo.java
@@ -0,0 +1,7 @@
+package com.example.RestAPI_IVIBO.Repositories;
+
+import com.example.RestAPI_IVIBO.Models.LichTrinh;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LichTrinhRepo extends JpaRepository<LichTrinh,Long> {
+}
